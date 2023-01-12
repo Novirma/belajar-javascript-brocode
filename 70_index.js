1111b1744fa228e43d9bd1a6d030a1979d1f9134
Novirma <raihannovirma@gmail.com>
2023-01-12 17:34:39 +0700

    Indak jaleh dek aa errornyo file ko

---
+++ 70_index.js
@@ -0,0 +1,10 @@
+import {PI, getCircumference, getArea} from "./70_math_util.js";
+// import * as MathUtil from "./70_math_util.js";
+
+console.log(PI);
+
+let circumference = MathUtil.getCircumference(10);
+console.log(circumference);
+
+let area = MathUtil.getArea(10);
+console.log(area);
